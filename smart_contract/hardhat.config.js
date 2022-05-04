5293addbee4f445ffaed377d5ae1c5b12e3eacec
thiji <76956269+thiji@users.noreply.github.com>
2022-05-04 23:15:45 +0700

    env

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -3,14 +3,15 @@
 //waffle for smart contract test
 require("@nomiclabs/hardhat-waffle");
 
+URL = process.env.URL;
+ACC = process.env.ACC;
+
 module.exports = {
   solidity: "0.8.0",
   networks: {
     ropsten: {
-      url: "https://eth-ropsten.alchemyapi.io/v2/FZcCitnqdROfnvIexY_f6gMODNpGpype",
-      accounts: [
-        "f54dd34c9a27cb06647239da13cee9eb6dcf83ac633c171264d9c35f965922e2",
-      ],
+      url: URL,
+      accounts: [ACC],
     },
   },
 };
